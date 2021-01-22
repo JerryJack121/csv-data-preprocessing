83464fd931560715c18236219f9da067be26b0e9
JerryJack121 <73692359+JerryJack121@users.noreply.github.com>
2021-01-22 21:27:01 +0800

    Update

--- prediction_for_lily_price_and_volume/concate_csv.py
+++ prediction_for_lily_price_and_volume/concate_csv.py
@@ -20,4 +20,4 @@ for set in sets:
     df10 = pd.read_csv(os.path.join(file_path, file_name[9]))
 
     data = pd.concat((df1, df2, df3, df4, df5, df6, df7, df8, df9, df10), axis=1)
-    data.to_csv(os.path.join(file_path, 'all.csv'))
+    data.to_csv(os.path.join(os.path.join(r'D:\dataset\lilium_price', set), (year+'all.csv')), header=None, index=None)
