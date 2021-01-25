b481ab924c877703ea6f8e71cd5dfc3c19e58a88
JerryJack121 <73692359+JerryJack121@users.noreply.github.com>
2021-01-25 13:22:53 +0800

    Update preprocessing.py

--- prediction_for_lily_price_and_volume/preprocessing.py
+++ prediction_for_lily_price_and_volume/preprocessing.py
@@ -22,52 +22,48 @@ def fill_df(df, lost_list, sum_price_high, sum_price_mid, sum_price_avg, sum_vol
     return df
 
 org_fold = r'D:\dataset\lilium_price\org' #原始資料
-fold = r'D:\dataset\lilium_price' #生成資料
-year = '104-108_879-899'
-
+fold = r'D:\dataset\lilium_price\generate_dataset' #生成資料
+year = '105-108_all'
+file_name ='FS443.xls'
 org_fold = os.path.join(org_fold, year)
-fold = os.path.join(fold, year)
-csv_list = os.listdir(org_fold)
-for file_name in csv_list:
-    org_path = os.path.join(org_fold, file_name)
-    df = pd.read_excel(org_path, header=4)
-    i = 0
-    predate, prehp, premp, preavg, pre_vol = datetime.datetime(2015, 2,13), 0, 0, 0, 0 #設定日期在開始日期的前一天
-    lost_list = []
-    sum_price_high, sum_price_mid, sum_price_avg, sum_volume =0, 0, 0, 0
-    with tqdm(total=len(df)-1) as pbar:
-        df = df.drop(['市　　場', '產　　品', '最高價', '下價', '增減%', '增減%.1', '殘貨量', 'Unnamed: 12'], axis=1) #刪除特定欄位
-        for index, row in df.iterrows():
-            data_row = df.loc[index].values
-            date, price_high, price_mid, price_avg, volume= data_row    #日期、上價、中價、平均價、交易量        
-            if date == '小　　計':
-                break
-            sum_price_high += price_high
-            sum_price_mid += price_mid
-            sum_price_avg +=price_avg
-            sum_volume += volume
-            year = int(date.split('/')[0]) + 1911
-            month = int(date.split('/')[1])
-            day = int(date.split('/')[2])
-            date = datetime.datetime(year, month, day)
-            df.loc[index, '日　　期'] = pd.to_datetime('%d/%d/%d'%(date.year, date.month, date.day))
-            interval = (date - predate).days
-            if not interval == 1: #前後兩個日期不相臨
-                for i in range(interval-1):    #找出中間所有缺少的日期
-                    lost_date = predate + datetime.timedelta(days = (i + 1))
-                    # lost_list.append([lost_date, (prehp+price_high)/2, (premp+price_mid)/2, (preavg+price_avg)/2, (pre_vol+volume)/2])
-                    lost_list.append(lost_date)
-            predate  = date
-            prehp = price_high
-            premp = price_mid
-            preavg = price_avg
-            pre_vol = volume
-            
+org_path = os.path.join(org_fold, file_name)
+df = pd.read_excel(org_path, header=4)
+i = 0
+predate, prehp, premp, preavg, pre_vol = datetime.datetime(2010, 12,31), 0, 0, 0, 0 #設定日期在開始日期的前一天
+lost_list = []
+sum_price_high, sum_price_mid, sum_price_avg, sum_volume =0, 0, 0, 0
+with tqdm(total=len(df)-1) as pbar:
+    df = df.drop(['市　　場', '產　　品', '最高價', '下價', '增減%', '增減%.1', '殘貨量', 'Unnamed: 12'], axis=1) #刪除特定欄位
+    for index, row in df.iterrows():
+        data_row = df.loc[index].values
+        date, price_high, price_mid, price_avg, volume= data_row    #日期、上價、中價、平均價、交易量        
+        if date == '小　　計':
+            break
+        sum_price_high += price_high
+        sum_price_mid += price_mid
+        sum_price_avg +=price_avg
+        sum_volume += volume
+        year = int(date.split('/')[0]) + 1911
+        month = int(date.split('/')[1])
+        day = int(date.split('/')[2])
+        date = datetime.datetime(year, month, day)
+        df.loc[index, '日　　期'] = pd.to_datetime('%d/%d/%d'%(date.year, date.month, date.day))
+        interval = (date - predate).days
+        if not interval == 1: #前後兩個日期不相臨
+            for i in range(interval-1):    #找出中間所有缺少的日期
+                lost_date = predate + datetime.timedelta(days = (i + 1))
+                # lost_list.append([lost_date, (prehp+price_high)/2, (premp+price_mid)/2, (preavg+price_avg)/2, (pre_vol+volume)/2])
+                lost_list.append(lost_date)
+        predate  = date
+        prehp = price_high
+        premp = price_mid
+        preavg = price_avg
+        pre_vol = volume
+        
 
+        # 更新進度條
+        pbar.update(1)
+        pbar.set_description(file_name)
 
-            # 更新進度條
-            pbar.update(1)
-            pbar.set_description(file_name)
-
-    df = fill_df(df, lost_list, sum_price_high, sum_price_mid, sum_price_avg, sum_volume)
-    df.to_csv(os.path.join(fold, file_name[:-4]+'.csv'), encoding='utf_8_sig', index=False)
+df = fill_df(df, lost_list, sum_price_high, sum_price_mid, sum_price_avg, sum_volume)
+df.to_csv(os.path.join(fold, file_name[:-4]+'.csv'), encoding='utf_8_sig', index=False)
